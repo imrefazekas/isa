893bc79c0a97ad4692cba5f613a347fa8a94b2e1
Imre Fazekas <imre.fazekas@icloud.com>
2017-12-29 10:36:47 +0100

    fixed libs

--- test/function.js
+++ test/function.js
@@ -3,7 +3,14 @@ let _ = require('../IsA')
 function shifted ( state, target = '*' ) {}
 function shifted2 ( state, ...args ) {}
 function shifted3 ( state, args={} ) {}
+function shifted4 ( state, args_123 = {} ) {}
+
+async function simple (greetings1, greetings2, terms, ignite) {}
 
 console.log( _.parameterNames( shifted ) )
 console.log( _.parameterNames( shifted2 ) )
 console.log( _.parameterNames( shifted3 ) )
+console.log( _.parameterNames( shifted4 ) )
+console.log( _.parameterNames( simple ) )
+
+console.log( 'args_123'.match( /([\\.A-Za-z]+)/g ) )
